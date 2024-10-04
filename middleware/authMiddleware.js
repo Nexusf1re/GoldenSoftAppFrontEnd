477ea30dcbdef3c785bae7fbe483e8a08205ef46
Nexusf1re <matteussantos30@hotmail.com>
2024-10-03 22:35:41 -0300

    testing authentication

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,28 +1,21 @@
+// Função para verificar se o token JWT está presente e válido
 function isAuthenticated() {
     const token = localStorage.getItem('token'); // ou sessionStorage
-    
+
     if (!token) {
       return false; // Token não existe
     }
     
     try {
-      const payload = jwt_decode(token); // Use a biblioteca jwt-decode para decodificar o token
-  
-      // Verifica se o token está expirado
-      const currentTime = Date.now() / 1000; // Tempo atual em segundos
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      const currentTime = Date.now() / 1000;
+
       if (payload.exp < currentTime) {
         return false; // Token expirado
       }
-      
+
       return true; // Token válido
     } catch (e) {
-      console.error("Erro ao decodificar o token:", e);
       return false; // Erro ao decodificar o token
     }
   }
-  
-  // Redirecionar se o usuário não estiver autenticado
-  if (!isAuthenticated()) {
-    window.location.href = "/index.html"; // Redireciona para a página de login
-  }
-  
