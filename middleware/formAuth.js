fba8c771434f253dd7246f52e7a6cd206479476b
Matteus <matteussantos30@gmail.com>
2024-10-07 09:22:32 -0300

    Auth form improved

--- middleware/formAuth.js
+++ middleware/formAuth.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const token = localStorage.getItem('token');
+    console.log('Token recuperado:', token); // Verifique se o token está presente
 
     if (!token) {
         window.location.href = 'index.html'; // Redireciona se não houver token
@@ -13,15 +14,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
     .then(response => {
-        if (response.ok) {
-            return response.text(); // Se o token for válido, obtém o conteúdo
-        } else {
+        console.log('Response Status:', response.status); // Verifique o status da resposta
+        if (!response.ok) {
             window.location.href = 'index.html'; // Redireciona se o token não for válido
         }
-    })
-    .then(html => {
-        // Insere o HTML diretamente no body
-        document.getElementById('formBody').innerHTML = html; // Carrega o conteúdo do formulário no body
     })
     .catch(error => {
         console.error('Erro ao acessar o formulário:', error);
