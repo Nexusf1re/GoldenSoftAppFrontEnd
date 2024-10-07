3900852876c044e60fc563f18bb0c58ce044ab7d
Matteus <matteussantos30@gmail.com>
2024-10-07 15:58:46 -0300

    entry page added

---
+++ components/nav.js
@@ -0,0 +1,19 @@
+const nav = `
+<ul>
+  <li class="list">
+    <a class="active" href="form.html">Formulário</a>
+  </li>
+ 
+  <li class="list">
+    <a class="active" href="entry.html">Lançamentos</a>
+  </li>
+  
+   <!--
+  <li class="list">
+    <a class="active" href="dashboard.html">Relatório</a>
+  </li>
+    -->
+</ul>
+`;
+
+document.getElementById('nav-container').innerHTML = nav;
---
+++ services/entry.js
@@ -0,0 +1,38 @@
+ // Função para buscar os dados da API e preencher a tabela
+ async function fetchData() {
+    try {
+        const response = await fetch('https://golden-soft-app-back-end.vercel.app/entry'); // Substitua pela URL da sua API
+        const data = await response.json();
+
+        populateTable(data);
+    } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+    }
+}
+
+// Função para preencher a tabela com os dados da API
+function populateTable(data) {
+    const table = document.getElementById('data-table');
+
+    data.forEach(row => {
+        const newRow = document.createElement('tr');
+
+        newRow.innerHTML = `
+            <td>${row.id}</td>
+            <td>${row.nome}</td>
+            <td>R$&nbsp;${row.valor.toFixed(2)}</td>
+            <td>${row.descricao}</td>
+            <td>${new Date(row.data).toLocaleDateString('pt-BR')}</td>
+            <td><button class="far fa-edit" onClick="editarRegistro('${row.id}')"></button></td>
+        `;
+
+        table.appendChild(newRow);
+    });
+}
+
+function editarRegistro(id) {
+    window.location.href = `editar.html?id=${id}`;
+}
+
+// Carrega os dados ao iniciar a página
+document.addEventListener('DOMContentLoaded', fetchData);
