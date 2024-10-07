2ec3ff1d7ee1136ade65c9f9f9078865f4ba1e1f
Matteus <matteussantos30@gmail.com>
2024-10-07 16:19:09 -0300

    conditional list

---
+++ services/movimentacoesGeral.js
@@ -0,0 +1,23 @@
+
+
+
+// Função para buscar movimentações do servidor
+async function fetchMovimentacoes() {
+    try {
+      const response = await fetch(API_URL+'/movimentacoesGeral');
+      const categorias  = await response.json();
+
+      const selectElementMovimentacoes = document.getElementById("pgto");
+
+     // selectElementTipo.innerHTML="";
+
+      categorias.forEach(categoria => {
+        const option = document.createElement("option");
+        option.value = categoria.categoria;  // Pega o valor correto do campo "categoria"
+        option.textContent = categoria.categoria;  // Exibe o texto correto no dropdown
+        selectElementMovimentacoes.appendChild(option);
+      });
+    } catch (error) {
+      console.error("Erro ao buscar movimentações:", error);
+    }
+  }
