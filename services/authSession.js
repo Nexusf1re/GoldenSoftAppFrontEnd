564f29865ada690f72adcf513aa70a46bd6a5553
Matteus <matteussantos30@gmail.com>
2024-10-08 07:54:55 -0300

    auth

--- services/authSession.js
+++ services/authSession.js
@@ -1,23 +1,6 @@
-const token = localStorage.getItem('token');
-
-if (!token) {
-    window.location.href = 'index.html'; // Redireciona para login se o token não existir
-} else {
-    fetch('https://golden-soft-app-back-end.vercel.app/auth', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            // Se o token for inválido ou expirado, redireciona para a página de login
-            window.location.href = 'index.html';
-        }
-    })
-    .catch(error => {
-        console.error('Erro ao verificar o token:', error);
-        window.location.href = 'index.html';
-    });
-}
+  // Verifica se o token existe no localStorage
+  const token = localStorage.getItem('token');
+  if (!token) {
+      // Se não existir, redireciona para a página de login
+      window.location.href = 'index.html';
+  }
