9ed4ac8f52e5f0aeb95fe1e3eb5760024d771afc
Matteus <matteussantos30@gmail.com>
2024-10-17 09:32:47 -0300

    entry.js

--- services/edit.js
+++ services/edit.js
@@ -39,14 +39,14 @@ async function fetchEntryData(id) {
 
 // Função para preencher o formulário com os dados obtidos
 function populateForm(data) {
-    document.getElementById('valorinn').value = data.valor || ''; 
-    document.getElementById('tipo').value = data.nome || ''; 
+    document.getElementById('valorinn').value = data.valor || '';
+    document.getElementById('tipo').value = data.nome || '';
     document.getElementById('observacao').value = data.observacao || '';
-    document.getElementById('data').value = new Date(data.data).toISOString().split('T')[0] || ''; 
+    document.getElementById('data').value = new Date(data.data).toISOString().split('T')[0] || '';
 
-    const movimentacaoCorreta = data.descricao; 
+    const movimentacaoCorreta = data.descricao;
     const selectElementMovimentacoes = document.getElementById("pgto");
-    selectElementMovimentacoes.value = movimentacaoCorreta || ''; 
+    selectElementMovimentacoes.value = movimentacaoCorreta || '';
 
     // Se o valor da movimentação não estiver na lista, adicione um log para verificar
     if (!Array.from(selectElementMovimentacoes.options).some(option => option.value === movimentacaoCorreta)) {
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Lida com a alteração no campo 'tipo'
-document.getElementById("tipo").addEventListener("change", async function() {
+document.getElementById("tipo").addEventListener("change", async function () {
     const selectedValue = this.value; // Captura o valor selecionado
 
     // Limpa as opções atuais do select de movimentações
@@ -98,7 +98,7 @@ async function updateEntry() {
         },
         body: JSON.stringify({ nome, valor, descricao, observacao, data, username }),
     });
-  
+
     if (response.ok) {
         const message = await response.text();
         Swal.fire({
@@ -135,74 +135,73 @@ async function updateEntry() {
 
 
 // Adiciona o listener de submissão do formulário
-document.getElementById("expenseForm").addEventListener("submit", function(event) {
+document.getElementById("expenseForm").addEventListener("submit", function (event) {
     event.preventDefault();
-    updateEntry(); 
+    updateEntry();
 });
 
 
-document.getElementById('deleteBtn').addEventListener("submit" , function(event){
+document.getElementById('deleteBtn').addEventListener("submit", function (event) {
     event.preventDefault();
     deleteEntry();
 });
 
 
-     function deleteEntry() {
-        const token = localStorage.getItem('token');
-        const username = localStorage.getItem('username');
+function deleteEntry() {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('username');
 
-        fetch(`${API_URL}/despesas/entryDelete/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({ username })
-        })
-            .then(response => {
-                if (response.ok) {
-                    const message = response.text();
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Sucesso',
-                        text: message,
-                        confirmButtonText: 'OK',
-                        confirmButtonColor: '#28a745', // Verde para sucesso
-                        background: '#fefefe',
-                        backdrop: `rgba(0,0,0,0.4)`,
-                    }).then(() => {
-                        // Limpa o formulário e redireciona para sucesso
-                        document.getElementById("expenseForm").reset();
-                        window.location.href = 'entry.html';
-                    });
-                } else {
-                    const errorMessage = response.text();
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Erro ao enviar os dados',
-                        text: errorMessage,
-                        confirmButtonText: 'OK',
-                        confirmButtonColor: '#d33',
-                        background: '#fefefe',
-                        backdrop: `rgba(0,0,0,0.4)`,
-                    }).then(() => {
-                        // Remove token e redireciona para login
-                        localStorage.removeItem('token');
-                        localStorage.removeItem('username');
-                        window.location.href = 'index.html';
-                    });
-                }
-            })
-            .catch(error => {
+    fetch(`${API_URL}/despesas/entryDelete/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ username })
+    })
+        .then(response => {
+            if (response.ok) {
+                const message = response.text();
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Sucesso',
+                    text: message,
+                    confirmButtonText: 'OK',
+                    confirmButtonColor: '#28a745', // Verde para sucesso
+                    background: '#fefefe',
+                    backdrop: `rgba(0,0,0,0.4)`,
+                }).then(() => {
+                    // Limpa o formulário e redireciona para sucesso
+                    document.getElementById("expenseForm").reset();
+                    window.location.href = 'entry.html';
+                });
+            } else {
+                const errorMessage = response.text();
                 Swal.fire({
                     icon: 'error',
-                    title: 'Erro',
-                    text: error.message,
+                    title: 'Erro ao enviar os dados',
+                    text: errorMessage,
                     confirmButtonText: 'OK',
                     confirmButtonColor: '#d33',
                     background: '#fefefe',
                     backdrop: `rgba(0,0,0,0.4)`,
-                    })
-        })
+                }).then(() => {
+                    // Remove token e redireciona para login
+                    localStorage.removeItem('token');
+                    localStorage.removeItem('username');
+                    window.location.href = 'index.html';
+                });
             }
-})
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Erro',
+                text: error.message,
+                confirmButtonText: 'OK',
+                confirmButtonColor: '#d33',
+                background: '#fefefe',
+                backdrop: `rgba(0,0,0,0.4)`,
+            })
+        })
+}
